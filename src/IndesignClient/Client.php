57f53be277b9d5e5febfeb4b68ac8ab3306e6a01
davidnjuko <david.ribes@njuko.com>
2014-05-25 19:24:40 +0200

    Improve Client in order to be detect ip and port form wsdl url

--- src/IndesignClient/Client.php
+++ src/IndesignClient/Client.php
@@ -14,12 +14,21 @@ class Client extends \SoapClient {
 
     private $scriptLanguage = 'javascript';
 
+    private $port = '12345';
+    private $ip = '127.0.0.1';
+
     function __construct($wsdl = 'http://127.0.0.1:12345/service?wsdl')
     {
+        preg_match('/[0-9.]+/', $wsdl, $ipMatches);
+        preg_match('/:[0-9]+/', $wsdl, $portMatches);
+
+        $this->setIp($ipMatches[0]);
+        $this->setPort(str_replace(":","",$portMatches[0]));
+
         $this->SoapClient($wsdl, array(
             'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP,
-            'proxy_port'  =>  12345,
-            'proxy_host'  =>  '192.168.9.129',
+            'proxy_port'  =>  $this->port,
+            'proxy_host'  =>  $ipMatches[0],
         ));
     }
 
@@ -67,4 +76,60 @@ class Client extends \SoapClient {
 
         return true;
     }
+
+    /**
+     * @param string $ip
+     * @return $this
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @param string $port
+     * @return $this
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param string $scriptLanguage
+     * @return $this
+     */
+    public function setScriptLanguage($scriptLanguage)
+    {
+        $this->scriptLanguage = $scriptLanguage;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getScriptLanguage()
+    {
+        return $this->scriptLanguage;
+    }
+
+
 }
